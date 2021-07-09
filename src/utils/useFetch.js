41d35f27f32cd9158391c24104baf318f35bf17f
TragedyHasStruck <tragedystruck@gmail.com>
2021-07-09 15:14:59 +0200

    fetch rewards on every submit to avoid stale values

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -1,24 +1,9 @@
-import { useEffect, useState } from "react";
 import { rewardsUrl } from "./addresses";
 
-function useFetch(url = rewardsUrl) {
-  const [response, setResponse] = useState(null);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(url);
-
-        const allRewards = await res.json();
-        setResponse(allRewards.rewards);
-      } catch (error) {
-        setError(error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  return [response, error];
+async function useFetch(url = rewardsUrl) {
+  const res = await fetch(url);
+  const allRewards = await res.json();
+  return allRewards.rewards;
 }
 
 export default useFetch;
